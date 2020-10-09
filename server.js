dee4c846c03bccc0abfaa597c6f5cb11e4750e53
Anyaegbu Francis <anyaegbufrancis@gmail.com>
2020-10-10 01:00:20 +0400

    server.js file

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
+//Requires the necessary module
 const express = require("express");
+const mongooseDB = require("mongoose")
+const Workout = require("./models/model")
 
 const PORT = process.env.PORT || 3000
 
@@ -9,8 +12,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-// routes
-app.use(require("./routes/routes.js"));
+// Requires routes
+app.use(require("./routes/api-routes.js"));
+app.use(require("./routes/html-routes.js"));
+
+//Connects to Mongoose DB
+mongooseDB.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true
+})
+.then(e => {console.log("Express Hooked up to MongoDB!!!")});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
